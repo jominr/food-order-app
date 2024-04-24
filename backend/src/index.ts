2a507084b5c3e5ee1e4cc65135f2b21cb1707400
jominr <jominr@qq.com>
2024-04-24 14:23:37 +1000

    Cloudinary setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose"
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+// cloudinary package exports a V2 variable which is the version 2 of their API, 
 
 // typescript : as string
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 .then(()=> console.log("Connected to database!"))
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 const app = express();
 
 // add the middleware: convert the req.body to json
