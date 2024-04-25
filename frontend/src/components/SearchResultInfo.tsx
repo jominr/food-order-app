fbdd449075c037ffdaef0f0cdc530bbe30935f97
jominr <jominr@qq.com>
2024-04-25 19:36:31 +1000

    cuisines filter && sort options filter

--- frontend/src/components/SearchResultInfo.tsx
+++ frontend/src/components/SearchResultInfo.tsx
@@ -7,13 +7,10 @@ type Props = {
 
 const SearchResultInfo = ({total, city} : Props) => {
   return (
-    <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
-      <span>
-        {total} Restaurants found in {city}
-        <Link to="/" className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500">Change Location</Link>
-      </span>
-      insert sort dropdown here
-    </div>
+    <span className="text-xl font-bold ">
+      {total} Restaurants found in {city}
+      <Link to="/" className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500">Change Location</Link>
+    </span>
   );
 };
 
